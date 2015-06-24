69ff748f89e61dddd26c2ac3e18d79d7a9709435
tstinghen <tstinghen@gmail.com>
2015-06-24 10:15:29 -0700

    Add about admin section

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   
   get 'browse/adminguide' 
   
+  get 'browse/aboutadmin'
+  
   get 'requests/closed'
 
   # The priority is based upon order of creation: first created -> highest priority.
