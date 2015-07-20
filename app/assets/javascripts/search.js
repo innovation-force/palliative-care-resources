be2a2cb4d8c65797d9756487dd47824498861b7e
Jonah George <jonah.george@me.com>
2015-07-20 10:55:31 -0700

    Removed unused javascript files

--- app/assets/javascripts/search.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  var searchBar = $("#tag_ids");
-  searchBar.select2({
-    placeholder: "Search CareLibrary"
-  });
-  searchBar.on("change", function (event) {
-    $(this).closest("form").submit();
-  });
-});
